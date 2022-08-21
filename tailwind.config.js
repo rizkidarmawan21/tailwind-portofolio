fdc0dbc1990efd6dadb7ffd79ff7a41f8b62292c
Rizki Darmawan <rizkidarmawan.0402102@gmail.com>
2022-08-22 00:00:33 +0700

    feat: add section about

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
     extend: {
       colors: {
         'primary': '#14b8a6',
-        'dark' : '#0f172a'
+        'dark': '#0f172a',
+        'secondary' :'#64748b'
       },
       screens: {
         '2xl': '1320px'
